30bb3fd04d5a3e3be56609d8b6538df4443eb5d5
romaTk <rt.craz@gmail.com>
2026-02-02 21:30:23 +0000

    Refactor MessageText class to improve resize handling; extract resize logic into dedicated methods for better readability and maintainability

--- src/ts/game/tasks/magic-words/dialogue/message/message-text/index.ts
+++ src/ts/game/tasks/magic-words/dialogue/message/message-text/index.ts
@@ -1,6 +1,3 @@
-/* eslint-disable max-statements */
-/* eslint-disable max-lines-per-function */
-/* eslint-disable one-var */
 import { LayoutContainer, LayoutSprite } from '@pixi/layout/components'
 import type { HTMLText } from 'pixi.js'
 
@@ -17,6 +14,10 @@ export class MessageText<
 	HTMLTextLike extends HTMLText,
 	PreciseSizeHelperLike extends TPreciseSizeHelper<HTMLTextLike>,
 > extends LayoutContainer {
+	// We need these flags to avoid multiple resize calls at the same time
+	protected isResizingStarted: boolean
+	protected isResizeAgainNeeded: boolean
+
 	// Used to set layout space for the text
 	protected readonly space: LayoutSprite
 	// The actual text message
@@ -24,19 +25,18 @@ export class MessageText<
 	protected readonly defaultWidth: number
 	protected readonly presiseSizeHelper: PreciseSizeHelperLike
 
-	protected isResizingStarted: boolean
-	protected isResizeAgainNeeded: boolean
-
 	public constructor(
 		text: HTMLTextLike,
 		presiseSizeHelper: PreciseSizeHelperLike,
 	) {
 		super()
+		this.isResizingStarted = false
+		this.isResizeAgainNeeded = false
 		this.text = text
 		this.defaultWidth = this.text.width
 		this.space = new LayoutSprite({
 			layout: {
-				height: 20,
+				height: 0,
 				maxWidth: '100%',
 				width: Math.ceil(this.defaultWidth),
 			},
@@ -57,52 +57,65 @@ export class MessageText<
 		this.space.layout = {
 			width: '100%',
 		}
+		// HACK We need to wait for layout to be updated and rendered in exacly this order
 		this.space.onRender = (): void => {
 			this.space.onRender = null
 			this.space.onLayout = (): void => {
+				// POSSIBLE_BUG - layout can be null here => read below
 				// @ts-expect-error - in types it requires function, but actually it can be null (what is better)
 				this.space.onLayout = null
-				const batchableHTMLText = this.getBatchableHTMLOfMessageText(),
-					defaultPosition = { [`x`]: 0, [`y`]: 0 }
-				this.text.position = {
-					[`x`]: this.space.layout?.realX ?? defaultPosition.x,
-					[`y`]: this.space.layout?.realY ?? defaultPosition.y,
-				}
-				if (!batchableHTMLText) {
-					// Don`t know how to handle this case properly
-					throw new Error('Batchable HTMLText data is not available')
-				}
-				batchableHTMLText.texturePromise
-					.then(() => {
-						if (this.space.width > this.defaultWidth) {
-							this.text.style.wordWrap = false
-							this.space.layout = {
-								width: this.defaultWidth,
-							}
-						} else {
-							this.text.style.wordWrap = true
-							this.text.style.wordWrapWidth = this.space.width
-							const preciseWidth =
-								this.presiseSizeHelper.getBoundingClientRect(
-									this.text,
-								).width
-							this.space.layout = {
-								width: preciseWidth,
-							}
-						}
-						this.space.layout = {
-							height: this.text.height,
-						}
-						this.isResizingStarted = false
-						if (this.isResizeAgainNeeded) {
-							this.resize()
-						}
-					})
-					.catch((err: unknown) => {
-						throw err
-					})
+				this.resizeAfterAllUpdated()
 			}
 			this.space.layout?.forceUpdate()
+		}
+	}
+
+	protected resizeAfterAllUpdated(): void {
+		const batchableHTMLText = this.getBatchableHTMLOfMessageText(),
+			defaultPosition = { [`x`]: 0, [`y`]: 0 }
+		this.text.position = {
+			[`x`]: this.space.layout?.realX ?? defaultPosition.x,
+			[`y`]: this.space.layout?.realY ?? defaultPosition.y,
+		}
+		if (!batchableHTMLText) {
+			// Don`t know how to handle this case properly
+			throw new Error('Batchable HTMLText data is not available')
+		}
+		batchableHTMLText.texturePromise
+			.then(() => {
+				if (this.space.width > this.defaultWidth) {
+					this.resizeForLargeWidth()
+				} else {
+					this.resizeForSmallWidth()
+				}
+				this.space.layout = {
+					height: this.text.height,
+				}
+				this.isResizingStarted = false
+				if (this.isResizeAgainNeeded) {
+					this.resize()
+				}
+			})
+			.catch((err: unknown) => {
+				throw err
+			})
+	}
+
+	protected resizeForLargeWidth(): void {
+		this.text.style.wordWrap = false
+		this.space.layout = {
+			width: this.defaultWidth,
+		}
+	}
+
+	protected resizeForSmallWidth(): void {
+		this.text.style.wordWrap = true
+		this.text.style.wordWrapWidth = this.space.width
+		const preciseWidth = this.presiseSizeHelper.getBoundingClientRect(
+			this.text,
+		).width
+		this.space.layout = {
+			width: preciseWidth,
 		}
 	}
 
